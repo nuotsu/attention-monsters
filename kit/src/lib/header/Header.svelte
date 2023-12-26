e13788ea81a68668c883e329ba4219ca22f3e69e
Mitchell Christ <xzmaru@gmail.com>
2023-12-26 11:45:05 -0800

    add gradient backdrop-blur

--- kit/src/lib/header/Header.svelte
+++ kit/src/lib/header/Header.svelte
@@ -1,6 +1,13 @@
-<header class="sticky z-10 top-0 p-4 mix-blend-difference" bind:offsetHeight>
+<header class="sticky z-10 top-0 p-4 after:backdrop-blur" bind:offsetHeight>
 	<a class="h3" href="/" data-sveltekit-reload>{$page.data.site.title}</a>
 </header>
+
+<style lang="postcss">
+	header::after {
+		mask-image: linear-gradient(to bottom, #000 25%, transparent);
+		@apply absolute inset-0 -bottom-8 z-[-1];
+	}
+</style>
 
 <script lang="ts">
 	import { page } from '$app/stores'
