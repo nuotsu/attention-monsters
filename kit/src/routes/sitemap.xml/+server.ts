f7e0f7c852e095b3c752aa585fbee9f33e1a019a
Mitchell Christ <xzmaru@gmail.com>
2024-01-08 21:20:59 -0800

    update sitemap

--- kit/src/routes/sitemap.xml/+server.ts
+++ kit/src/routes/sitemap.xml/+server.ts
@@ -1,6 +1,13 @@
+import { client } from '$utils/sanity'
+import groq from 'groq'
+
 const BASE_URL = 'https://attentionmonsters.com'
 
-export function GET() {
+export async function GET() {
+	const pages = await client.fetch<string[]>(
+		groq`*[_type == 'page' && metadata.slug.current != 'index'].metadata.slug.current`,
+	)
+
 	return new Response(
 		`<?xml version="1.0" encoding="UTF-8"?>
 		<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -9,6 +16,16 @@ export function GET() {
 				<changefreq>weekly</changefreq>
 				<priority>1.0</priority>
 			</url>
+			${pages
+				.map(
+					(slug) =>
+						`<url>
+					<loc>${BASE_URL}/${slug}</loc>
+					<changefreq>weekly</changefreq>
+					<priority>0.9</priority>
+				</url>`,
+				)
+				.join('')}
 		</urlset>`.trim(),
 		{
 			headers: { 'content-type': 'application/xml' },
