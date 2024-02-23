460a5ceb5f1055d14f4e3ef78fd44e3129972a16
Mitchell Christ <xzmaru@gmail.com>
2024-02-22 20:00:59 -0800

    minor adjustments

--- kit/src/lib/modules/EmbeddedPlayer.svelte
+++ kit/src/lib/modules/EmbeddedPlayer.svelte
@@ -1,22 +1,20 @@
-<section class="relative">
-	<div
-		class="section grid grid-cols-[var(--cols)] gap-4 sm:gap-8 max-sm:carousel"
-		style:--cols={items.filter((item) => item.enabled).length}
-	>
-		{#each items as item}
-			{#if item.enabled}
-				<article
-					class="shrink-0 w-full sm:max-w-[650px] mx-auto"
-					style="--size: min(88vw, 300px)"
-				>
-					<h2 class="sr-only">{item.title}</h2>
+<div
+	class="gap-4 sm:gap-8 carousel before:m-auto after:m-auto"
+	style:--cols={items.filter((item) => item.enabled).length}
+>
+	{#each items as item}
+		{#if item.enabled}
+			<article
+				class="shrink-0 md:[--max:640px]"
+				style:--size="min(88vw, var(--max, 300px))"
+			>
+				<h2 class="sr-only">{item.title}</h2>
 
-					{@html item.code}
-				</article>
-			{/if}
-		{/each}
-	</div>
-</section>
+				{@html item.code}
+			</article>
+		{/if}
+	{/each}
+</div>
 
 <script lang="ts">
 	const { items = [] } = $$props as Partial<{
